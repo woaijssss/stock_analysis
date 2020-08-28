608c11864e0d445d251476eac14fa1fb2ffb002f
wenhanf@163.com <wenhanf@163.com>
2020-08-28 10:16:41 +0800

    增加datas目录创建

--- bin/main.py
+++ bin/main.py
@@ -1,5 +1,10 @@
 
 from stock_analysis.src.StockService import StockService
+import os
+from pathlib import Path
 
 if __name__ == '__main__':
-    StockService().startService()
+    dataPath = Path("../datas")
+    if not dataPath.is_dir():   # datas目录不存在，则创建（因git不上传数据文件，因此git clone之后没有datas目录）
+        # StockService().startService()
+        dataPath.mkdir()
