dbe967936d2a0874a47bba1234ad107e5e252469
wenhanf@163.com <wenhanf@163.com>
2020-09-14 12:21:47 +0800

    修复程序执行bug

--- basic/company_information_collection/collector.py
+++ basic/company_information_collection/collector.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from pathlib import Path
 import pandas as pd
 from pandas import DataFrame
 import tushare as ts
@@ -10,6 +11,10 @@ pro = ts.pro_api(token='e5ccd9b1da858f2e127afef26431dd550ebd8d837f2394816722f0f9
 
 root_path = "./datas/"
 stock_info_file = "A_当前上市交易的股票列表.xlsx"
+
+dataPath = Path(root_path)
+if not dataPath.is_dir():  # datas目录不存在，则创建（因git不上传数据文件，因此git clone之后没有datas目录）
+    dataPath.mkdir()
 
 df_StockInfo = None
 if stock_info_file in os.listdir(root_path):    # 文件存在
@@ -22,8 +27,8 @@ else:
 
 compony_basic_file = "上市公司基本信息.xlsx"
 df_company_basic = None
-if stock_info_file in os.listdir("./datas"):    # 文件存在
-    print(stock_info_file, "存在")
+if compony_basic_file in os.listdir("./datas"):    # 文件存在
+    print(compony_basic_file, "存在")
     df_company_basic = pd.read_excel(root_path + compony_basic_file, sheet_name='公司信息')
 
 else:
@@ -39,9 +44,9 @@ if company_datas_file in os.listdir("./datas"):    # 文件存在
     df_company_datas = pd.read_excel(root_path + company_datas_file, sheet_name='公司数据信息')
 else:
     df_company_datas = ts.get_stock_basics()
-    print(df_company_datas.columns)
+    # print(df_company_datas.columns)
     df_company_datas = NounsEng2Chn().converseEng2Chn(df_company_datas, NounsEng2Chn.mDataCompanyBasicInfo)
-    print(df_company_datas.columns)
-    df_company_datas.to_excel(root_path + company_datas_file, sheet_name='公司信息', index=False)
+    # print(df_company_datas.columns)
+    df_company_datas.to_excel(root_path + company_datas_file, sheet_name='公司数据信息', index=False)
 
 
